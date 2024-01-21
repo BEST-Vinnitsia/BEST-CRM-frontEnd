42e6f70d2578003a60a28c1c9ad48b00d966e775
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2024-01-21 20:43:56 +0200

    changed on not required onClick

--- src/pages/Membership.tsx
+++ src/pages/Membership.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { BreadcrumbsContainer, Button } from '../components';
+import { PATH_MEMBERSHIP } from '../routes/paths';
+import { SvgClose } from '../assets/svg';
 
 export default function MembershipPage() {
-    return <></>;
+    return (
+        <>
+            <BreadcrumbsContainer
+                path={[
+                    { url: PATH_MEMBERSHIP.ROOT, title: 'membership' },
+                    { url: PATH_MEMBERSHIP.LIST, title: 'list' },
+                ]}
+            >
+                <Button svg={<SvgClose />} title="test" />
+            </BreadcrumbsContainer>
+        </>
+    );
 }
