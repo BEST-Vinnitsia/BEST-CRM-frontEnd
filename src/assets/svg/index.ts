deae3f18805bc996900094fb1a5efe6f54d06c60
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2024-01-27 19:27:06 +0200

    moved svg

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,27 +1,34 @@
-export { ReactComponent as SvgLogo } from './logo.svg';
+// best
+export { ReactComponent as SvgLogo } from './best/logo.svg';
+export { ReactComponent as SvgBESTLogo } from './best/bestLogo.svg';
+export { ReactComponent as SvgVinny } from './best/vinny.svg';
+
+// alerts
+export { ReactComponent as SvgError } from './alerts/error.svg';
+export { ReactComponent as SvgWarn } from './alerts/warn.svg';
+export { ReactComponent as SvgSuccess } from './alerts/success.svg';
+export { ReactComponent as SvgInfo } from './alerts/info.svg';
+
+// arrows
+export { ReactComponent as SvgArrow } from './arrows/arrow.svg';
+
+// checkbox
+export { ReactComponent as SvgCheckboxInactive } from './checkbox/checkboxInactive.svg';
+export { ReactComponent as SvgCheckboxActive } from './checkbox/checkboxActive.svg';
+export { ReactComponent as SvgCheckboxClear } from './checkbox/checkboxClear.svg';
+
+// loaders
+export { ReactComponent as SvgCircle } from './loaders/circle.svg';
+
+// password
+export { ReactComponent as SvgHiddenPassword } from './password/hiddenPassword.svg';
+export { ReactComponent as SvgVisiblePassword } from './password/visiblePassword.svg';
+
+// other
 export { ReactComponent as SvgMenu } from './menu.svg';
-export { ReactComponent as SvgArrow } from './arrow.svg';
 export { ReactComponent as SvgUser } from './user.svg';
-
-// Snackbar
-export { ReactComponent as SvgError } from './error.svg';
-export { ReactComponent as SvgWarn } from './warn.svg';
-export { ReactComponent as SvgSuccess } from './success.svg';
-export { ReactComponent as SvgInfo } from './info.svg';
-
 export { ReactComponent as SvgClose } from './close.svg';
 
-// Logo
-export { ReactComponent as SvgBESTLogo } from './bestLogo.svg';
-export { ReactComponent as SvgVinny } from './vinny.svg';
-
-// Password
-export { ReactComponent as SvgHiddenPassword } from './hiddenPassword.svg';
-export { ReactComponent as SvgVisiblePassword } from './visiblePassword.svg';
-
-// loading
-export { ReactComponent as SvgCircle } from './circle.svg';
-
-export { ReactComponent as SvgCheckboxInactive } from './checkboxInactive.svg';
-export { ReactComponent as SvgCheckboxActive } from './checkboxActive.svg';
-export { ReactComponent as SvgCheckboxClear } from './checkboxClear.svg';
+//
+//
+//
