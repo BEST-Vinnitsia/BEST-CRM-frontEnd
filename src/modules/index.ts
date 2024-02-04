d7e3bfe72297b5ba65293f2989d2899b87a387de
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2024-02-04 16:43:28 +0200

    delete modules filder

--- src/modules/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Header } from '../components/header/Header';
-
-// sidebar
-export { default as DesktopSidebar } from '../components/sidebar/desktopSidebar';
-export { default as MobileSidebar } from '../components/sidebar/mobileSidebar';
