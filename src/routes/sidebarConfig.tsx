0a412efe962ebe2f2e064730a869ee14235ce012
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2024-03-24 21:41:19 +0200

    sort svg

--- src/routes/sidebarConfig.tsx
+++ src/routes/sidebarConfig.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { PATH_COMMITTEE, PATH_DASHBOARD, PATH_EVENT, PATH_INFO, PATH_MEMBER } from './paths';
-import { SvgCommitteeSidebar, SvgDashboard, SvgEventSidebar, SvgInfoSidebar, SvgMembers } from '../assets/svg';
+import { SvgCommittee, SvgDashboard, SvgEvent, SvgInfoSidebar, SvgMembers } from '../assets/svg';
 
 export const sidebarConfig = [
     { title: 'Dashboard', path: PATH_DASHBOARD.ROOT, svg: <SvgDashboard /> },
-    { title: 'Event categories', path: PATH_EVENT.CATEGORY.LIST, svg: <SvgEventSidebar /> },
-    { title: 'Event', path: PATH_EVENT.LIST, svg: <SvgEventSidebar /> },
     { title: 'Members', path: PATH_MEMBER.LIST, svg: <SvgMembers /> },
-    { title: 'Committee', path: PATH_COMMITTEE.LIST, svg: <SvgCommitteeSidebar /> },
+    { title: 'Committee', path: PATH_COMMITTEE.LIST, svg: <SvgCommittee /> },
+    { title: 'Event', path: PATH_EVENT.LIST, svg: <SvgEvent /> },
+    { title: 'Event categories', path: PATH_EVENT.CATEGORY.LIST, svg: <SvgEvent /> },
     { title: 'Info', path: PATH_INFO.ROOT, svg: <SvgInfoSidebar /> },
 ];
